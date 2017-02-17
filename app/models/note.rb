7f8045e92f1547b615799fdbe5cf41258d60b0d2
waka <kakutawaka@gmail.com>
2017-02-17 16:27:42 +0900

    add edit&delete to timeline

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,7 +3,6 @@ class Note < ActiveRecord::Base
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
 
-  validates :title, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
 
