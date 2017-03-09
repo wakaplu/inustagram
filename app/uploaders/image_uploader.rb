6fc84cbf371f18c550634b6da3e533358c52c849
waka <kakutawaka@gmail.com>
2017-03-09 19:10:40 +0900

    change imageuploader resie process

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,13 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :resized do
-    # returns an image with a maximum width of 100px
-    # while maintaining the aspect ratio
-    # 10000 is used to tell CW that the height is free
-    # and so that it will hit the 100 px width first
-    process :resize_to_fit => [1024, 1024]
-  end
+  process :resize_to_fit => [1024, 1024]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
